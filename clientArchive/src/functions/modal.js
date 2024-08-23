bd62fda975548d6e5fc09a383432e6739c10b6b6
ArtemH92 <a.hominski@yandex.ru>
2024-08-23 19:48:58 +0300

    delete client archive

--- clientArchive/src/functions/modal.js
+++
@@ -1,9 +0,0 @@
-import { ref } from "vue";
-
-export const ModalState = ref(false)
-export const ModalValue = ref('')
-
-export const hendlerModal = (state, value) => {
-  ModalState.value = state
-  ModalValue.value = value
-}
--- clientArchive/src/layouts/NotAuthLayout.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <div class="min-h-screen bg-fuchsia-500 flex justify-center items-center">
-    <router-view />
-  </div>
-</template>
