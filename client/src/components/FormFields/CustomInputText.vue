d3d58a15f14dc0043f459feaa9fec515a39b0672
ArtemH92 <a.hominski@yandex.ru>
2024-08-23 22:26:46 +0300

    add validation for create user page

---
+++ client/src/components/FormFields/CustomInputText.vue
@@ -0,0 +1,29 @@
+<script setup>
+import FloatLabel from 'primevue/floatlabel'
+import InputText from 'primevue/inputtext'
+
+const model = defineModel()
+const props = defineProps({
+  label: String,
+  disabled: Boolean,
+  errors: String
+})
+</script>
+
+<template>
+  <div>
+    <FloatLabel>
+      <InputText
+        v-model="model"
+        class="w-full"
+        :invalid="errors != ''"
+        :disabled="props.disabled"
+        :aria-describedby="`${model}-error`"
+      />
+      <label>{{ props.label }}</label>
+    </FloatLabel>
+    <small :id="`${model}-error`" class="text-red-500">
+      {{ errors }}
+    </small>
+  </div>
+</template>
